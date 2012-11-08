f199d62f04ff905edcd2028dea7a6ffb96199c20
lmm-git <leomarschke@arcor.de>
2012-11-08 18:45:17 +0100

    Add basic admin functions

---
+++ src/modules/Beam/lib/Beam/Controller/Admin.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Beam admin area
+ *
+ * @copyright  (c) Leonard Marschke
+ * @license    GPLv3
+ * @package    Beam/Admin
+ */
+class Beam_Controller_Geocoding extends Zikula_AbstractController
+{
+	/**
+	 * @brief Give overview of all admin jobs
+	 * @return string HTML
+	 *
+	 * @author Leonard Marschke
+	 * @version 0.1
+	 * @todo all
+	 */
+	public function main()
+	{
+		return '<h1>Coming soon</h1>';
+	}
+	
+	/**
+	 * @brief Beamer overview
+	 * @return string HTML
+	 *
+	 * Display overview with all jobs for the displays. Excessive use of JavaScript
+	 *
+	 * @author Leonard Marschke
+	 * @version 0.1
+	 * @todo all
+	 */
+	public function viewDisplays()
+	{
+		return '<h1>Coming soon</h1>';
+	}
+	
+	/**
+	 * @brief Add and edit display
+	 * @return string HTML
+	 *
+	 * Add and edit display form with all avaiable jobs. It is intended to use the FormUtil
+	 *
+	 * @author Leonard Marschke
+	 * @version 0.1
+	 * @todo all
+	 */
+	public function configureDisplay()
+	{
+		return '<h1>Coming soon</h1>';
+	}
+	
+	/**
+	 * @brief Add and edit display
+	 * @return string HTML
+	 *
+	 * Remove display. The security question should be done by JavaScript.
+	 *
+	 * @author Leonard Marschke
+	 * @version 0.1
+	 * @todo all
+	 */
+	public function removeDisplay()
+	{
+		return '<h1>Coming soon</h1>';
+	}
+	
+	/**
+	 * @brief Give overview of all jobs
+	 * @return string HTML
+	 *
+	 * Overview of all avaiable jobs for the Displays. With standard checkbox.
+	 *
+	 * @author Leonard Marschke
+	 * @version 0.1
+	 * @todo all
+	 */
+	public function viewJobs()
+	{
+		return '<h1>Coming soon</h1>';
+	}
+	
+	/**
+	 * @brief Add and edit function
+	 * @return string HTML
+	 *
+	 * Add and edit function form with all avaiable jobs. It is intended to use the FormUtil
+	 *
+	 * @author Leonard Marschke
+	 * @version 0.1
+	 * @todo all
+	 */
+	public function configureJob()
+	{
+		return '<h1>Coming soon</h1>';
+	}
+	
+	/**
+	 * @brief Remove function
+	 * @return string HTML
+	 *
+	 * Remove function. The security question should be done by JavaScript.
+	 *
+	 * @author Leonard Marschke
+	 * @version 0.1
+	 * @todo all
+	 */
+	public function removeJob()
+	{
+		return '<h1>Coming soon</h1>';
+	}
+}
